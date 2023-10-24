adeeadff2ad043d6ddb89a14bce87fadceac0b35
fabiojunik <fabiojunik@gmail.com>
2023-10-24 22:13:54 +0100

    refactor(quiz): not create quiz if always exists

--- qui/src/modules/quiz/entities/quiz.entity.ts
+++ qui/src/modules/quiz/entities/quiz.entity.ts
@@ -1,10 +1,8 @@
-import crypto from 'crypto';
-
 export class Quiz {
   id: string;
   name: string;
 
-  constructor(props: { name: string }, id: string) {
-    this.id = id ?? crypto?.randomUUID();
+  constructor(props: { name: string }, id?: string) {
+    Object.assign(this, props);
   }
 }
--- qui/src/modules/quiz/use-cases/create-quiz.usecase.ts
+++ qui/src/modules/quiz/use-cases/create-quiz.usecase.ts
@@ -1,6 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, BadRequestException } from '@nestjs/common';
 import { CreateQuizDto } from '../dto/create-quiz.dto';
 import { IQuizRepository } from '../repositories/IQuiz-repository';
+import { Quiz } from '../entities/quiz.entity';
+import crypto from 'crypto';
 
 @Injectable()
 export class CreateQuizUseCase {
@@ -10,6 +12,12 @@ export class CreateQuizUseCase {
   ) {}
 
   async execute({ name }: CreateQuizDto) {
-    await this.quizRepository.create({ name });
+    const quizExists = await this.quizRepository.findByName(name);
+
+    if (quizExists)
+      throw new BadRequestException('Quiz já registado com esse nome');
+
+    const quiz = new Quiz({ name });
+    await this.quizRepository.create(quiz);
   }
 }
